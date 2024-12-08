aeba301f5d2b733c7f5058edcd2d811d03fb81c5
Juan Esteban <159485717+JU4NESZz@users.noreply.github.com>
2024-12-08 12:14:13 -0500

    Mi primer commit

---
+++ Trabajo1/src/Primero/Main.java
@@ -0,0 +1,29 @@
+package Primero;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		/*LEA: A, B
+		SI A > B ENTONCES
+		ESCRIBA: «A ES MAYOR QUE B»
+		SINO
+		SI A = B ENTONCES
+		ESCRIBA: «A ES IGUAL A B»
+		SINO
+		ESCRIBA: «A ES MENOR QUE B»
+		FIN_SI
+		FIN_SI
+		FIN_INICIO*/
+	
+ Scanner scanner = new Scanner(System.in);
+System.out.println("Ingresa el valor de A: \n" );		
+ int A = scanner.nextInt();
+ System.out.println("Ingresa el valor de B: \n" );	
+ int B = scanner.nextInt();
+ 
+ System.out.println("Valores de A: " + A + " Valores de B " + B );	
+	
+
+	}
+
+}
