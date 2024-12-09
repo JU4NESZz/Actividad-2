14ce00b690070c852980772e7fb794439393b05d
Juan Esteban <159485717+JU4NESZz@users.noreply.github.com>
2024-12-09 10:42:45 -0500

    Ultima correccion, segunda actividad

--- Trabajo1/src/Segundo/Main.java
+++ Trabajo1/src/Segundo/Main.java
@@ -1,10 +1,25 @@
 package Segundo;
-
+import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Estudiante Juan = new Estudiante("0001", "Juan Pablo", 1500000.0, 4);
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese el numero de inscripcion: ");
+		String NI = scanner.nextLine();
+		System.out.println("Ingrese el nombre del estudiante: ");
+		String N = scanner.nextLine();
+		scanner.nextLine();
+		System.out.println("Ingrese el patrimonio del estudiante: ");
+		double P = scanner.nextDouble();
+		System.out.println("Ingrese el numero de inscripcion: ");
+		scanner.nextLine();
+		int ES = scanner.nextInt(); 
+		
+		Estudiante juanPablo = new Estudiante( NI, N, P, ES);
+		
+		
+		//Estudiante Juan = new Estudiante("0001", "Juan Pablo", 1500000.0, 4);
 	}
 
 }
---
+++ Trabajo1/src/Tercero/Main.java
@@ -0,0 +1,9 @@
+package Tercero;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
