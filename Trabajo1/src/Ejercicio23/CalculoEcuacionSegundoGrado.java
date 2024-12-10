21120353ca65b76d7f3a20b79e4316272b1be26d
jjuanr <jurinconba@unal.edu.co>
2024-12-09 20:02:41 -0500

    Update CalculoEcuacionSegundoGrado.java

--- Trabajo1/src/Ejercicio23/CalculoEcuacionSegundoGrado.java
+++ Trabajo1/src/Ejercicio23/CalculoEcuacionSegundoGrado.java
@@ -3,21 +3,7 @@ package Ejercicio23;
 public class CalculoEcuacionSegundoGrado {
 
     public static void calcularSoluciones(double a, double b, double c) {
-
-        if (b == 0 && c <= 0) {
-            if (c < 0) {
-                System.out.println("Soluciones de la ecuación:");
-                System.out.println("x1 = " + Math.sqrt(-c / a));
-                System.out.println("x2 = " + -Math.sqrt(-c / a));
-            } else {
-                System.out.println("Solución:");
-                System.out.println("x = 0");
-            }
-        } else if (b == 0 && c > 0) {
-            System.out.println("La ecuación no tiene solución en los números reales.");
-        } else {
             double discriminante = Math.pow(b, 2) - (4 * a * c);
-
             if (discriminante > 0) {
                 System.out.println("Soluciones de la ecuación:");
                 System.out.println("x1 = " + ((-b + Math.sqrt(discriminante)) / (2 * a)));
@@ -28,6 +14,6 @@ public class CalculoEcuacionSegundoGrado {
             } else {
                 System.out.println("La ecuación no tiene solución en los números reales.");
             }
-        }
+        
     }
 }
