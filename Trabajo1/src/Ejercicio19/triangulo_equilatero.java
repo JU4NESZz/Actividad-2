aee5158f7e01bcfdab238f4742f6d378fc7d6b38
jjuanr <jurinconba@unal.edu.co>
2024-12-09 19:40:02 -0500

    Update triangulo_equilatero.java

--- Trabajo1/src/Ejercicio19/triangulo_equilatero.java
+++ Trabajo1/src/Ejercicio19/triangulo_equilatero.java
@@ -3,13 +3,13 @@ package Ejercicio19;
 public class triangulo_equilatero {
 	
 	
-	public static double calcular_altura(double lado); {
+	public static double calcular_altura(double lado) {
         return (lado + Math.sqrt(3)) / 2;
     }
-    public static double calcular_perimetro(double lado); {
+    public static double calcular_perimetro(double lado) {
         return lado * 3;
     }
-    public static double calcular_area(double lado); {
+    public static double calcular_area(double lado) {
         return (Math.pow(lado, 2) * Math.sqrt(3)) / 4;
     }
 }
