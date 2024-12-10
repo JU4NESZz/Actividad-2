b44ac36265016027a5fa0949d8863ba93366574d
jjuanr <jurinconba@unal.edu.co>
2024-12-09 19:56:28 -0500

    Update Empleado.java

--- Trabajo1/src/Ejercicio22/Empleado.java
+++ Trabajo1/src/Ejercicio22/Empleado.java
@@ -5,7 +5,7 @@ public class Empleado {
 	public static double calcular_salario(double salario_hora, int numero_horas) {
 	        return salario_hora * numero_horas;
 	}
-	public static empleado(double salario, String nombre) {
+	public static void mostrar_informacion_empleado(double salario, String nombre) {
 	        if(salario > 450000){
 	            System.out.println("Nombre del empleado: " + nombre);
 	            System.out.println("Salario de empleado: " + salario);
