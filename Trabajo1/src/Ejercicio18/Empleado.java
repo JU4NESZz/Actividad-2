8177fcd79bd9b2704b0973b0bcbead71cdcae5b6
jjuanr <jurinconba@unal.edu.co>
2024-12-09 19:32:39 -0500

    Update Empleado.java

--- Trabajo1/src/Ejercicio18/Empleado.java
+++ Trabajo1/src/Ejercicio18/Empleado.java
@@ -1,13 +1,13 @@
 package Ejercicio18;
 public class Empleado{
     
-    public static double calcular_porcentaje_retencion(double retencion_fuente); {
+    public static double calcular_porcentaje_retencion(double retencion_fuente) {
         return retencion_fuente/100;
     }
-    public static double calcular_salario_bruto(double valor_horas, int horas_trabajadas); {
+    public static double calcular_salario_bruto(double valor_horas, int horas_trabajadas {
         return valor_horas * horas_trabajadas;
     }
-    public static double calcular_salario_neto(double salario_bruto, double retencion_fuente); {
+    public static double calcular_salario_neto(double salario_bruto, double retencion_fuente) {
         return salario_bruto - (porcentaje_retencion * salario_bruto);
     }
 }
